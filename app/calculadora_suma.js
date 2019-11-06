436bae1ce10338187f6bf8db92bf11d1c300e86b
Daniel Ballesteros Bedoya <danielsballes@gmail.com>
2019-11-06 17:40:01 -0500

    Se agrega la prueba numero 1, realizada por Daniel Ballesteros usando assert.typeof

---
+++ app/calculadora_suma.js
@@ -0,0 +1,4 @@
+exports.sumaNumeros = (valor1, valor2) => {
+    var resultado = valor1 + valor2;
+    return resultado;
+}
---
+++ test/TestCalculadora.js
@@ -0,0 +1,11 @@
+var assert  = require("chai").assert;
+var calculadora = require("../app/calculadora_suma");
+
+describe("Calculadora de dos numeros: ", () => {
+	describe("Checkeo de la funcion sumaNumeros: ", () => {
+		it("Verificación de la suma usando: assert.typeOf(valor,'valor'): ", () => {
+			result = calculadora.sumaNumeros(2,2);
+			assert.typeOf(result, 'number');
+		});		
+	});		
+});
